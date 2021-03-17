30c787de0eca00a7fa2c7cd46e1b77017f38044a
renancaraujo <renan.araujo@dashdash.com>
2021-03-17 20:49:19 +0000

    flutter create again

--- android/app/src/main/kotlin/com/example/trex/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/trex/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.trex
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+
 }
